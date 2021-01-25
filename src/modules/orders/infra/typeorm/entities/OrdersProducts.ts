19ed6bb1d818fc129d096b1e808b4e6f8b66adcd
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-01-25 19:48:50 -0300

    Challenge Database Relations Complete

--- src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
@@ -21,7 +21,7 @@ class OrdersProducts {
   order: Order;
 
   @ManyToOne(() => Product, product => product.order_products)
-  @JoinColumn({name: 'order_id'})
+  @JoinColumn({name: 'product_id'})
   product: Product;
 
   @Column()
